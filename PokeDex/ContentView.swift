bad6b62be4ee62e2a10f56d1117cdd498c49030b
rathodvinod5 <rathodvinod5@gmail.com>
2025-06-24 23:30:39 +0530

    Built fetch service and fetch function to fetch pokemon from api

--- PokeDex/ContentView.swift
+++ PokeDex/ContentView.swift
@@ -15,6 +15,8 @@ struct ContentView: View {
         sortDescriptors: [NSSortDescriptor(keyPath: \Pokemon.id, ascending: true)],
         animation: .default)
     private var pokedex: FetchedResults<Pokemon>
+    
+    let fetcher = FetchService()
 
     var body: some View {
         NavigationView {
@@ -32,12 +34,39 @@ struct ContentView: View {
                     EditButton()
                 }
                 ToolbarItem {
-                    Button("Add Item", systemImage: "plus") {}
+                    Button("Add Item", systemImage: "plus") {
+                        getPokemon()
+                    }
                 }
             }
         }
     }
 
+    private func getPokemon() {
+        Task {
+            for id in 1..<152 {
+                do {
+                    let fetchedPokemon = try await fetcher.fetchPokemon(id)
+                    let pokemon = Pokemon(context: viewContext)
+                    pokemon.id = fetchedPokemon.id
+                    pokemon.name = fetchedPokemon.name
+                    pokemon.types = fetchedPokemon.types
+                    pokemon.hp = fetchedPokemon.hp
+                    pokemon.attack = fetchedPokemon.attack
+                    pokemon.defense = fetchedPokemon.defense
+                    pokemon.specialAttack = fetchedPokemon.specialAttack
+                    pokemon.specialDefence = fetchedPokemon.specialDefense
+                    pokemon.speed = fetchedPokemon.speed
+                    pokemon.sprite = fetchedPokemon.sprite
+                    pokemon.shiny = fetchedPokemon.shiny
+                    
+                    try viewContext.save()
+                } catch {
+                    print(error)
+                }
+            }
+        }
+    }
 }
 
 #Preview {
