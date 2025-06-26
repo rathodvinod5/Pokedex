18e3f1062e406a19d4c57b82ff7e162edc90e7a3
rathodvinod5 <rathodvinod5@gmail.com>
2025-06-26 23:31:56 +0530

    design 3 pokemon widgets

--- PokedexWidget/PokedexWidget.swift
+++ PokedexWidget/PokedexWidget.swift
@@ -36,33 +36,95 @@ struct Provider: TimelineProvider {
 
 struct SimpleEntry: TimelineEntry {
     let date: Date
-    let emoji: String
-    let type: [String]
+    let name: String
+    let types: [String]
     let sprite: Image
     
     static var placeholder: SimpleEntry {
         SimpleEntry(
             date: .now,
-            emoji: "bulbasaur",
-            type: ["grass", "poison"],
+            name: "bulbasaur",
+            types: ["grass", "poison"],
             sprite: Image(.bulbasaur))
     }
     
     static var placeholder2: SimpleEntry {
         SimpleEntry(
             date: .now,
-            emoji: "mew",
-            type: ["psychic"],
+            name: "mew",
+            types: ["psychic"],
             sprite: Image(.mew))
     }
 }
 
 struct PokedexWidgetEntryView : View {
+    @Environment(\.widgetFamily) var widgetSize
+    
     var entry: Provider.Entry
+    
+    var pokemonImage: some View {
+        entry.sprite
+            .interpolation(.none)
+            .resizable()
+            .scaledToFit()
+            .shadow(color: .black, radius: 6)
+    }
+    
+    var typesView: some View {
+        ForEach(entry.types, id: \.self) { type in
+            Text(type.capitalized)
+                .font(.subheadline)
+                .fontWeight(.semibold)
+                .foregroundStyle(.black)
+                .padding(.horizontal, 13)
+                .padding(.vertical, 5)
+                .background(Color(type.capitalized))
+                .clipShape(Capsule())
+                .shadow(radius: 3)
+        }
+    }
 
     var body: some View {
-        VStack {
-            entry.sprite
+        switch widgetSize {
+        case .systemLarge:
+            ZStack {
+                pokemonImage
+                
+                VStack(alignment: .leading) {
+                    Text(entry.name.capitalized)
+                        .font(.largeTitle)
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.75)
+                    
+                    Spacer()
+                    
+                    HStack {
+                        Spacer()
+                        typesView
+                    }
+                }
+            }
+        case .systemMedium:
+            HStack {
+                pokemonImage
+                
+                Spacer()
+                
+                VStack(alignment: .leading) {
+                    Text(entry.name.capitalized)
+                        .font(.title)
+                        .padding(.vertical, 1)
+                    
+                    HStack {
+                        typesView
+                    }
+                }
+                .layoutPriority(1)
+                
+                Spacer()
+            }
+        default: pokemonImage
+            
         }
     }
 }
@@ -74,19 +136,34 @@ struct PokedexWidget: Widget {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             if #available(iOS 17.0, *) {
                 PokedexWidgetEntryView(entry: entry)
-                    .containerBackground(.fill.tertiary, for: .widget)
+                    .foregroundStyle(.black)
+                    .containerBackground(Color(entry.types[0].capitalized), for: .widget)
             } else {
                 PokedexWidgetEntryView(entry: entry)
                     .padding()
                     .background()
             }
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .configurationDisplayName("Pokemon")
+        .description("See a random Pokemon")
     }
 }
 
 #Preview(as: .systemSmall) {
+    PokedexWidget()
+} timeline: {
+    SimpleEntry.placeholder
+    SimpleEntry.placeholder2
+}
+
+#Preview(as: .systemMedium) {
+    PokedexWidget()
+} timeline: {
+    SimpleEntry.placeholder
+    SimpleEntry.placeholder2
+}
+
+#Preview(as: .systemLarge) {
     PokedexWidget()
 } timeline: {
     SimpleEntry.placeholder
